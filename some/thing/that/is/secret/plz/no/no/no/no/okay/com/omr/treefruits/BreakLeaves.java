8b86de09c6497cca4c14825503a9c6b3eb7b4ead
AmaZiX <amazichannel@gmail.com>
2015-09-30 20:34:11 +0700

    Update something new...

--- some/thing/that/is/secret/plz/no/no/no/no/okay/com/omr/treefruits/BreakLeaves.java
+++ some/thing/that/is/secret/plz/no/no/no/no/okay/com/omr/treefruits/BreakLeaves.java
@@ -1,5 +1,7 @@
 package com.omr.treefruits;
 
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -14,16 +16,64 @@ public class BreakLeaves implements Listener {
         Double percent = Math.random();
         Player p = e.getPlayer();
         if(e.getBlock().getType() == Material.LEAVES){
-            if(percent < 0.75){
-                p.getInventory().addItem(Fruits.peach());
-            } else if (percent < 0.8) {
-                p.getInventory().addItem(Fruits.plum());
-            } else if (percent < 0.9) {
-                p.getInventory().addItem(Fruits.orange());
-            } else if (percent < 0.95) {
-                p.getInventory().addItem(Fruits.passion());
-            } else {
+            if(!(p.hasPermission("treefruits.bypass") || p.isOp())) {
 
+                if (!(p.getGameMode() == GameMode.CREATIVE)) {
+                    if (percent < 0.05) {
+                        p.getInventory().addItem(new ItemStack(Material.SAPLING));
+                        e.getBlock().getDrops().clear();
+                    } else if (percent < 0.1) {
+                        p.getInventory().addItem(new ItemStack(Material.APPLE));
+                        e.getBlock().getDrops().clear();
+                    } else if (percent < 0.25) {
+                        p.getInventory().addItem(Fruits.lychee());
+                        e.getBlock().getDrops().clear();
+                    } else if (percent < 0.5){
+                        p.getInventory().addItem(Fruits.mango());
+                        e.getBlock().getDrops().clear();
+                    } else if (percent < 0.75) {
+                        p.getInventory().addItem(Fruits.peach());
+                            e.getBlock().getDrops().clear();
+                    } else if (percent < 0.8) {
+                        p.getInventory().addItem(Fruits.plum());
+                            e.getBlock().getDrops().clear();
+                    } else if (percent < 0.9) {
+                        p.getInventory().addItem(Fruits.orange());
+                        e.getBlock().getDrops().clear();
+                    } else if (percent < 0.95) {
+                        p.getInventory().addItem(Fruits.passion());
+                        e.getBlock().getDrops().clear();
+                    }
+                } else {
+                    e.setCancelled(true);
+                    p.sendMessage(ChatColor.BLUE + "BreakLeaves> " + ChatColor.RED + "You need to be in survival mode in order to break leaves");
+                }
+            } else {
+                if (percent < 0.05) {
+                    p.getInventory().addItem(new ItemStack(Material.SAPLING));
+                    e.getBlock().getDrops().clear();
+                } else if (percent < 0.1) {
+                    p.getInventory().addItem(new ItemStack(Material.APPLE));
+                    e.getBlock().getDrops().clear();
+                } else if (percent < 0.25) {
+                    p.getInventory().addItem(Fruits.lychee());
+                    e.getBlock().getDrops().clear();
+                } else if (percent < 0.5){
+                    p.getInventory().addItem(Fruits.mango());
+                    e.getBlock().getDrops().clear();
+                } else if (percent < 0.75) {
+                    p.getInventory().addItem(Fruits.peach());
+                    e.getBlock().getDrops().clear();
+                } else if (percent < 0.8) {
+                    p.getInventory().addItem(Fruits.plum());
+                    e.getBlock().getDrops().clear();
+                } else if (percent < 0.9) {
+                    p.getInventory().addItem(Fruits.orange());
+                    e.getBlock().getDrops().clear();
+                } else if (percent < 0.95) {
+                    p.getInventory().addItem(Fruits.passion());
+                    e.getBlock().getDrops().clear();
+                }
             }
         }
     }
